facf30e3f35325bcd42dcaec7859e4cd66d1f5cf
SigmaChad <qinghangcom@gmail.com>
2024-11-12 15:33:56 +0800

    Particlesss
    Fixed margins, moved links to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,10 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import {Navigation} from "@/components/navigation";
-import {Martian_Mono} from 'next/font/google';
+import {Kalnia_Glaze} from 'next/font/google';
+import Link from "next/link";
 
-const martianMono = Martian_Mono({ subsets: ['latin'], weight: "500"})
+const martianMono = Kalnia_Glaze({ subsets: ['latin'], weight: "600"})
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -33,9 +34,30 @@ export default function RootLayout({
     >
     <div className="bg-gradient-to-br from-slate-900 to-slate-800">
       <header
-          className={"h-fit px-2 py-4 text-left text-4xl text-transparent bg-clip-text bg-gradient-to-tr from-cyan-200 to-pink-600 drop-shadow-2xl"}>
-        <h1 className={martianMono.className}>Easybutton Studios</h1>
-
+          className={"flex justify-between items-center h-fit px-2 py-0.5 text-left text-4xl text-transparent bg-clip-text bg-gradient-to-tr from-amber-100 to-yellow-500 drop-shadow-lg hover: drop-shadow-2x1 hover:bg-gradient-to-tr hover:from-amber-200 hover:to-yellow-700"}>
+        <h1 className={martianMono.className}>
+          <Link href="/">Easybutton Studios</Link>
+        </h1>
+        <div className="space-x-6 align-top scale-75 translate-x-6">
+          <a href="https://easybutton-studios.itch.io/">
+            <button
+                className="bg-slate-950 text-slate-100 border-2 border-amber-300 shadow-md my-2 px-1.5 py-2.5 hover:-translate-y-0.5 transition duration-200">
+              🛒
+            </button>
+          </a>
+          <a href="http://x.com/EZBTN_Studios">
+            <button
+                className="bg-slate-950 text-slate-100 border-2 border-amber-300 shadow-md my-2 py-2.5 px-5 hover:-translate-y-0.5 transition duration-200">
+              𝕏
+            </button>
+          </a>
+          <a href="https://www.youtube.com/@EasybuttonStudiosOfficial">
+            <button
+                className="bg-slate-950 text-slate-100 border-2 border-amber-300 shadow-md my-2 px-1.5 py-2.5 hover:-translate-y-0.5 transition duration-200">
+              ▶️
+            </button>
+          </a>
+        </div>
       </header>
     </div>
     <Navigation/>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,26 @@
-import { Figtree } from 'next/font/google'
+import { Kalnia_Glaze } from 'next/font/google'
+import ParticleCanvas from '../components/particles.js';
 
-const titleFont = Figtree({
+const titleFont = Kalnia_Glaze({
     subsets: ['latin'],
     display: 'swap',
+    weight: "600",
 })
 
 export default function Home() {
-  return (
-    <div className="bg-gradient-to-r from-slate-900 to-slate-800 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start text-center -translate-y-16">
-        <div className={`${titleFont.className}`}>
-            <div>
-                <h1
-                    className="text-7xl font-bold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-transparent bg-gradient-to-br from-cyan-500 to-fuchsia-500 bg-clip-text hover:drop-shadow-md transition duration-200">
-                    Easybutton Studios
-                </h1>
-                <h2 className="text-2xl font-bold text-slate-200 p-2">
-                    top 10 skibidi
-                </h2>
-                <div className="space-x-5 p-4">
-                    <a href="https://easybutton-studios.itch.io/">
-                        <button
-                            className="bg-slate-950 text-slate-100 border-2 border-cyan-300 shadow-md rounded-full my-2 px-3 py-2.5 hover:-translate-y-0.5 transition duration-200">
-                            🛒
-                        </button>
-                    </a>
-                    <a href="http://x.com/EZBTN_Studios">
-                        <button
-                            className="bg-slate-950 text-slate-100 border-2 border-cyan-300 shadow-md rounded-full my-2 py-2.5 px-4 hover:-translate-y-0.5 transition duration-200">
-                            𝕏
-                        </button>
-                    </a>
-                    <a href="https://www.youtube.com/@EasybuttonStudiosOfficial">
-                        <button
-                            className="bg-slate-950 text-slate-100 border-2 border-cyan-300 shadow-md rounded-full my-2 px-3 py-2.5 hover:-translate-y-0.5 transition duration-200">
-                            ▶️
-                        </button>
-                    </a>
+    return (
+        <div className="relative bg-gradient-to-r from-slate-900 to-slate-800 min-h-screen">
+            <ParticleCanvas />
+            <main className="relative z-10 flex flex-col gap-8 items-center justify-center text-center pt-32">
+                <div className={`${titleFont.className}`}>
+                    <h1 className="text-8xl font-bold p-4 text-transparent bg-gradient-to-br from-amber-200 to-amber-400 bg-clip-text">
+                        Easybutton Studios
+                    </h1>
                 </div>
-            </div>
-
+                <a href="/projects">
+                    <button className="font-mono bg-black text-amber-50 rounded-lg border-2 text-3xl p-2 hover:bg-slate-800 transition duration-200">EXPLORE</button>
+                </a>
+            </main>
         </div>
-      </main>
-    </div>
-
-  );
+    );
 }
